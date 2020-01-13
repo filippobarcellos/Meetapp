eafd4de4a935e531ebd0efbe347baced833b28b8
Filippo Barcellos <barcellos.filippo@gmail.com>
2020-01-13 21:17:39 +0100

    challenge 01

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token was not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+// 1 - Check if the token was sent
+// 2 - Get the token from header
+// 3 - Decoded and pass userId to request
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'filippobarcellos',
+  password: '',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
